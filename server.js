c8ac06525a52c02e04e0cefb456b40f2f6f660b9
Franz Ivan De Villa <franzivan.devilla@gmail.com>
2025-09-25 18:40:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+
+// Enable CORS for freeCodeCamp testing
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -57,4 +65,24 @@ console.log(result);
 
 
 
-app.listen(process.env.PORT || 3000, () => {});
+// API endpoints for freeCodeCamp testing
+app.get('/_api/package.json', (req, res) => {
+  const fs = require('fs');
+  res.json(JSON.parse(fs.readFileSync('./package.json', 'utf8')));
+});
+
+app.get('/_api/server.js', (req, res) => {
+  const fs = require('fs');
+  res.type('text/plain');
+  res.send(fs.readFileSync('./server.js', 'utf8'));
+});
+
+// Basic route to handle GET requests to root path
+app.get('/', (req, res) => {
+  res.send('<h1>BCrypt Example</h1><p>Check the console to see the bcrypt hashes being generated!</p>');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Listening on port:", PORT)
+});
